310c32e35e4ed83bba30331471379812ba215bab
Shaista Tosif <syedtauseef1972@gmail.com>
2024-11-30 18:49:02 +0500

    repaired

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -16,11 +16,11 @@ const Navbar = () => {
       <a className="mr-5 hover:text-green-400 font-sans">Certification</a>
       <a className="mr-5 hover:text-green-400 font-sans">Contact Info</a>
     </nav>
-    <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-green-400 rounded text-base mt-4 md:mt-0 font-sans text-black">Download Cv
+    
       <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 ml-1" viewBox="0 0 24 24">
         <path d="M5 12h14M12 5l7 7-7 7"></path>
       </svg>
-    </button>
+    
   </div>
 </header>
     </div>
